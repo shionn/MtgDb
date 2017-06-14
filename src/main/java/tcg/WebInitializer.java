82e27f51e7783f7e9f167adf09cf5fa35e210188
Shionn <shionn@gmail.com>
2017-06-14 13:26:37 +0200

    Activation Spring

---
+++ src/main/java/tcg/WebInitializer.java
@@ -0,0 +1,68 @@
+package tcg;
+
+
+import java.util.TimeZone;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Code sous licence GPLv3 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author <b>Shionn</b>, shionn@gmail.com <i>http://shionn.org</i><br>
+ *         GCS d- s+:+ a+ C++ UL/M P L+ E--- W++ N K- w-- M+ t+ 5 X R+ !tv b+ D+ G- e+++ h+ r- y+
+ */
+@Configuration
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Paris"));
+		super.onStartup(servletContext);
+	}
+
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { WebMvcConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	public void customizeRegistration(ServletRegistration.Dynamic registration) {
+		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+	}
+
+	@EnableWebMvc
+	@Configuration
+	@ComponentScan({ "tcg" })
+	public class WebMvcConfig extends WebMvcConfigurerAdapter {
+		@Bean
+		public InternalResourceViewResolver viewResolver() {
+			InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+			viewResolver.setViewClass(JstlView.class);
+			viewResolver.setPrefix("/WEB-INF/jsp/");
+			viewResolver.setSuffix(".jsp");
+			return viewResolver;
+		}
+	}
+}
