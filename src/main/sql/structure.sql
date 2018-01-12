dc4e88afe3267684c8f3d9d8b5e0d574435d39a9
lkepka <ludovic.kepka@ca-titres.fr>
2018-01-12 16:51:15 +0100

    Structure

--- src/main/sql/structure.sql
+++ src/main/sql/structure.sql
@@ -11,13 +11,31 @@ create table edition (
 create table card (
   id            varchar(32)   NOT NULL,
   edition       varchar(4)    NOT NULL,
-  number        varchar(4)    NOT NULL,
+  number        varchar(4)    NULL,
 
-  mana_cost     varchar(16)   NOT NULL,
+  name          varchar(128)  NOT NULL,
+  text          varchar(1024) NOT NULL,
+  flavor        varchar(1024) NOT NULL,
+
+  type          varchar(32)   NOT NULL,
+
+  mana_cost     varchar(32)   NOT NULL,
+  cmc           int(11),
+
   multiverse_id int(11),
   PRIMARY  KEY (id),
   UNIQUE   KEY multiverse_id(multiverse_id),
-  UNIQUE   KEY edition_number(edition, number),
+  CONSTRAINT card_edition FOREIGN KEY (edition) REFERENCES edition(code)
+) DEFAULT  CHARSET=utf8 AUTO_INCREMENT=1;
+
+create table card_lang (
+  id            varchar(32)   NOT NULL,
+  lang          varchar(2)    NOT NULL,
+  multiverse_id int(11),
+  name          varchar(128)  NOT NULL,
+
+  PRIMARY  KEY (id),
+  UNIQUE   KEY multiverse_id(multiverse_id),
   CONSTRAINT card_edition FOREIGN KEY (edition) REFERENCES edition(code)
 ) DEFAULT  CHARSET=utf8 AUTO_INCREMENT=1;
 
